6f3c2a941f837e3f3f5809f0d84c2eb539beb2f9
clementfavarel <clementfavarel.dev@gmail.com>
2023-11-25 14:54:40 +0100

    added sql dump

---
+++ src/database/dumps/dump-v1.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 24, 2023 at 10:33 PM
+-- Server version: 10.6.12-MariaDB-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mapollon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artiste`
+--
+
+CREATE TABLE `artiste` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `pseudo` varchar(50) DEFAULT NULL,
+  `telephone` int(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `web` varchar(255) NOT NULL,
+  `facebook` varchar(255) NOT NULL,
+  `instagram` varchar(255) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
+  `code_postal` int(5) NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `domaine` varchar(100) NOT NULL,
+  `presentation` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `num_stand` int(11) NOT NULL,
+  `photo_url` varchar(255) NOT NULL,
+  `interview_url` varchar(255) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(100) NOT NULL,
+  `siecle` varchar(3) NOT NULL,
+  `origine` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oeuvre`
+--
+
+CREATE TABLE `oeuvre` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `date` date DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `photo_url` varchar(255) NOT NULL,
+  `musique_url` varchar(255) NOT NULL,
+  `artiste_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reaction`
+--
+
+CREATE TABLE `reaction` (
+  `id` int(11) NOT NULL,
+  `oeuvre_id` int(11) NOT NULL,
+  `utilisateur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mdp` varchar(60) NOT NULL,
+  `tranche_age` varchar(5) NOT NULL,
+  `departement` varchar(50) NOT NULL,
+  `metier` varchar(50) NOT NULL,
+  `photo_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artiste`
+--
+ALTER TABLE `artiste`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artiste_genre_fk` (`genre_id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oeuvre`
+--
+ALTER TABLE `oeuvre`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oeuvre_artiste_fk` (`artiste_id`),
+  ADD KEY `oeuvre_genre_fk` (`genre_id`);
+
+--
+-- Indexes for table `reaction`
+--
+ALTER TABLE `reaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reaction_oeuvre_fk` (`oeuvre_id`),
+  ADD KEY `reaction_utilisateur_fk` (`utilisateur_id`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artiste`
+--
+ALTER TABLE `artiste`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `oeuvre`
+--
+ALTER TABLE `oeuvre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reaction`
+--
+ALTER TABLE `reaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `artiste`
+--
+ALTER TABLE `artiste`
+  ADD CONSTRAINT `artiste_genre_fk` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `oeuvre`
+--
+ALTER TABLE `oeuvre`
+  ADD CONSTRAINT `oeuvre_artiste_fk` FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `oeuvre_genre_fk` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reaction`
+--
+ALTER TABLE `reaction`
+  ADD CONSTRAINT `reaction_oeuvre_fk` FOREIGN KEY (`oeuvre_id`) REFERENCES `oeuvre` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `reaction_utilisateur_fk` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
