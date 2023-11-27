4c5f1ae4a633824777034a4b6d6a46211d74dc62
clementfavarel <clementfavarel.dev@gmail.com>
2023-11-27 18:58:45 +0100

    added auth pages and styles

---
+++ src/interfaces/pages/auth/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Connexion | Mapollon</title>
+    <?php include('../../../assets/includes/head.php'); ?>
+    <link rel="stylesheet" href="auth.css" />
+</head>
+
+<body>
+    <div class="card"></div>
+</body>
+
+</html>
