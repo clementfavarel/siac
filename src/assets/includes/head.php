9319c37a22f351a4109941eff2a9ffeda115592e
clementfavarel <plkode.dev@protonmail.com>
2023-12-05 15:49:29 +0100

    updated path of icons

--- src/assets/includes/head.php
+++ src/assets/includes/head.php
@@ -1,17 +1,12 @@
-<?php
-if (isset($_SESSION['user'])) {
-    header("Location: ../pages/map/");
-}
-?>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!-- FAVICONS -->
-<link rel="apple-touch-icon" sizes="180x180" href="/src/assets/icons/apple-touch-icon.png" />
-<link rel="icon" type="image/png" sizes="32x32" href="/src/assets/icons/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/src/assets/icons/favicon-16x16.png" />
-<link rel="manifest" href="/src/assets/icons/site.webmanifest" />
-<link rel="mask-icon" href="/src/assets/icons/safari-pinned-tab.svg" color="#2b5797" />
-<link rel="shortcut icon" href="/src/assets/icons/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="../../../assets/icons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="../../..//assets/icons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="../../..//assets/icons/favicon-16x16.png" />
+<link rel="manifest" href="../../..//assets/icons/site.webmanifest" />
+<link rel="mask-icon" href="../../..//assets/icons/safari-pinned-tab.svg" color="#2b5797" />
+<link rel="shortcut icon" href="../../..//assets/icons/favicon.ico" />
 <meta name="msapplication-TileColor" content="#2b5797" />
-<meta name="msapplication-config" content="/src/assets/icons/browserconfig.xml" />
+<meta name="msapplication-config" content="../../..//assets/icons/browserconfig.xml" />
 <meta name="theme-color" content="#ffffff" />
