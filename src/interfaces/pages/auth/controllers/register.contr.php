75810ef50f17ffb6c0e1dcfed0313e1b006b95ff
clementfavarel <plkode.dev@protonmail.com>
2023-12-05 14:33:32 +0100

    register backend finished

---
+++ src/interfaces/pages/auth/controllers/register.contr.php
@@ -0,0 +1,128 @@
+<?php
+
+function isEmailValid($email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function isPwdValid($pwd)
+{
+    return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+{};:,<.>§~]).{8,}$/', $pwd);
+}
+
+function createDate($day, $month, $year)
+{
+    return date('d-m-Y', strtotime($day . '-' . $month . '-' . $year));
+}
+
+function calculateAge($birthdate)
+{
+    $birthdateObj = DateTime::createFromFormat('d-m-Y', $birthdate);
+    if (!$birthdateObj) {
+        return false;
+    }
+
+    $currentDateObj = new DateTime();
+    $ageInterval = $currentDateObj->diff($birthdateObj);
+    $age = $ageInterval->y;
+
+    return $age;
+}
+
+/**
+ * REGISTERING USER
+ */
+
+$requiredFields = ['firstname', 'lastname', 'job', 'email', 'dd', 'mm', 'yyyy', 'pwd', 'pwd-repeat'];
+
+foreach ($requiredFields as $field) {
+    if (empty($_POST[$field])) {
+        header("Location: ../register.php?error=empty_fields");
+        exit();
+    }
+}
+
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    header("Location: ../register.php?error=invalid_email");
+    exit();
+}
+
+if (strlen($_POST['firstname']) > 50 || strlen($_POST['lastname']) > 50 || strlen($_POST['job']) > 50) {
+    header("Location: ../register.php?error=input_too_long");
+    exit();
+}
+
+$day = $_POST['dd'];
+$month = $_POST['mm'];
+$year = $_POST['yyyy'];
+
+if (!checkdate($month, $day, $year)) {
+    header("Location: ../register.php?error=invalid_date");
+    exit();
+}
+
+$date = createDate($day, $month, $year);
+$age = calculateAge($date);
+
+switch ($age) {
+    case ($age > 0 && $age < 16):
+        $ageGroup = '-16';
+        break;
+    case ($age >= 16 && $age < 25):
+        $ageGroup = '16-24';
+        break;
+    case ($age >= 25 && $age < 35):
+        $ageGroup = '25-34';
+        break;
+    case ($age >= 35 && $age < 45):
+        $ageGroup = '35-44';
+        break;
+    case ($age > 45 && $age < 65):
+        $ageGroup = '45-64';
+        break;
+    case ($age >= 65):
+        $ageGroup = '65+';
+        break;
+    default:
+        break;
+}
+
+if (!isPwdValid($_POST['pwd'])) {
+    header("Location: ../register.php?error=invalid_pwd");
+    exit();
+}
+
+if ($_POST['pwd'] !== $_POST['pwd-repeat']) {
+    header("Location: ../register.php?error=pwd_mismatch");
+    exit();
+}
+
+$hashedPwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+
+include_once('../../../../database/config/config.php');
+
+$nom = $_POST['firstname'];
+$prenom = $_POST['lastname'];
+$email = $_POST['email'];
+$mdp = $hashedPwd;
+$tranche_age = $ageGroup;
+$metier = $_POST['job'];
+
+$sql = "INSERT INTO utilisateur (nom, prenom, email, mdp, tranche_age, metier) VALUES (?, ?, ?, ?, ?, ?);";
+$stmt = $db->prepare($sql);
+$stmt->bindValue(1, $nom);
+$stmt->bindValue(2, $prenom);
+$stmt->bindValue(3, $email);
+$stmt->bindValue(4, $mdp);
+$stmt->bindValue(5, $tranche_age);
+$stmt->bindValue(6, $metier);
+$stmt->execute();
+
+// connect the user
+session_start();
+$_SESSION['user'] = $db->lastInsertId();
+$_SESSION['firstname'] = $nom;
+$_SESSION['lastname'] = $prenom;
+
+header("Location: ../../map?user_id=" . $_SESSION['user']);
+exit();
--- src/interfaces/pages/auth/register.php
+++ src/interfaces/pages/auth/register.php
@@ -18,137 +18,137 @@ TODO : Check if all steps are built accordingly to the database table utilisateu
     <title>Inscription | Mapollon</title>
     <?php include('../../../assets/includes/head.php'); ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../../../assets/css/global.css"/>
-    <link rel="stylesheet" href="assets/css/auth.css"/>
-    <link rel="stylesheet" href="assets/css/register.css"/>
+    <link rel="stylesheet" href="../../../assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/auth.css" />
+    <link rel="stylesheet" href="assets/css/register.css" />
 </head>
 
 <body>
-<div class="previous-page-btn">
-    <a href="../../../../index.php">
-        <i class="bi bi-chevron-left"></i>
-    </a>
-</div>
-<div class="container">
+    <div class="previous-page-btn">
+        <a href="../../../../index.php">
+            <i class="bi bi-chevron-left"></i>
+        </a>
+    </div>
+    <div class="container">
 
 
-    <h1>Inscription</h1>
-    <form method="post" id="regForm" action="controllers/auth.contr.php">
-        <!-- One "tab" for each step in the form: -->
-        <div class="tab">
-            <label for="firstname">Prénom</label>
-            <input oninput="this.className = ''" name="firstname"/>
-            <label for="lastname">Nom</label>
-            <input oninput="this.className = ''" name="lastname"/>
-        </div>
-        <div class="tab">
-            <label for="job">Profession</label>
-            <input oninput="this.className = ''" name="job"/>
-            <label for="email">Email</label>
-            <input type="email" oninput="this.className = ''" name="email"/>
-        </div>
-        <div class="tab">
-            <label for="birth">Date de naissance</label>
-            <div class="birth">
-                <input placeholder="jj" oninput="this.className = ''" name="dd"/>
-                <input placeholder="mm" oninput="this.className = ''" name="mm"/>
-                <input placeholder="aaaa" oninput="this.className = ''" name="yyyy"/>
+        <h1>Inscription</h1>
+        <form method="post" id="regForm" action="controllers/register.contr.php">
+            <!-- One "tab" for each step in the form: -->
+            <div class="tab">
+                <label for="firstname">Prénom</label>
+                <input oninput="this.className = ''" name="firstname" />
+                <label for="lastname">Nom</label>
+                <input oninput="this.className = ''" name="lastname" />
             </div>
-        </div>
-        <div class="tab">
-            <label for="username">Mot de passe</label>
-            <input oninput="this.className = ''" name="pwd" type="password"/>
-            <label for="username">Confirmer le mot de passe</label>
-            <input oninput="this.className = ''" name="pwd-repeat" type="password"/>
-        </div>
-        <!-- Circles which indicates the steps of the form: -->
-        <div class="dots">
-            <span class="step"></span>
-            <span class="step"></span>
-            <span class="step"></span>
-            <span class="step"></span>
-        </div>
-        <div class="btns">
-            <button class="btn primary" type="button" id="nextBtn" onclick="nextPrev(1)">Suivant</button>
-            <button class="btn secondary" type="button" id="prevBtn" onclick="nextPrev(-1)">Précédent</button>
-        </div>
-    </form>
-</div>
+            <div class="tab">
+                <label for="job">Profession</label>
+                <input oninput="this.className = ''" name="job" />
+                <label for="email">Email</label>
+                <input type="email" oninput="this.className = ''" name="email" />
+            </div>
+            <div class="tab">
+                <label for="birth">Date de naissance</label>
+                <div class="birth">
+                    <input placeholder="jj" oninput="this.className = ''" name="dd" />
+                    <input placeholder="mm" oninput="this.className = ''" name="mm" />
+                    <input placeholder="aaaa" oninput="this.className = ''" name="yyyy" />
+                </div>
+            </div>
+            <div class="tab">
+                <label for="username">Mot de passe</label>
+                <input oninput="this.className = ''" name="pwd" type="password" />
+                <label for="username">Confirmer le mot de passe</label>
+                <input oninput="this.className = ''" name="pwd-repeat" type="password" />
+            </div>
+            <!-- Circles which indicates the steps of the form: -->
+            <div class="dots">
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+            </div>
+            <div class="btns">
+                <button class="btn primary" type="button" id="nextBtn" onclick="nextPrev(1)">Suivant</button>
+                <button class="btn secondary" type="button" id="prevBtn" onclick="nextPrev(-1)">Précédent</button>
+            </div>
+        </form>
+    </div>
 
-<script>
-    var currentTab = 0; // Current tab is set to be the first tab (0)
-    showTab(currentTab); // Display the current tab
+    <script>
+        var currentTab = 0; // Current tab is set to be the first tab (0)
+        showTab(currentTab); // Display the current tab
 
-    function showTab(n) {
-        // This function will display the specified tab of the form ...
-        var x = document.getElementsByClassName("tab");
-        x[n].style.display = "flex";
-        // ... and fix the Previous/Next buttons:
-        if (n == 0) {
-            document.getElementById("prevBtn").style.display = "none";
-        } else {
-            document.getElementById("prevBtn").style.display = "inline";
-        }
-        if (n == (x.length - 1)) {
-            document.getElementById("nextBtn").innerHTML = "S'inscrire";
-        } else {
-            document.getElementById("nextBtn").innerHTML = "Suivant";
-        }
-        // ... and run a function that displays the correct step indicator:
-        fixStepIndicator(n)
-    }
-
-    function nextPrev(n) {
-        // This function will figure out which tab to display
-        var x = document.getElementsByClassName("tab");
-        // Exit the function if any field in the current tab is invalid:
-        if (n == 1 && !validateForm()) return false;
-        // Hide the current tab:
-        x[currentTab].style.display = "none";
-        // Increase or decrease the current tab by 1:
-        currentTab = currentTab + n;
-        // if you have reached the end of the form... :
-        if (currentTab >= x.length) {
-            //...the form gets submitted:
-            document.getElementById("regForm").submit();
-            return false;
-        }
-        // Otherwise, display the correct tab:
-        showTab(currentTab);
-    }
-
-    function validateForm() {
-        // This function deals with validation of the form fields
-        var x, y, i, valid = true;
-        x = document.getElementsByClassName("tab");
-        y = x[currentTab].getElementsByTagName("input");
-        // A loop that checks every input field in the current tab:
-        for (i = 0; i < y.length; i++) {
-            // If a field is empty...
-            if (y[i].value == "") {
-                // add an "invalid" class to the field:
-                y[i].className += " invalid";
-                // and set the current valid status to false:
-                valid = false;
+        function showTab(n) {
+            // This function will display the specified tab of the form ...
+            var x = document.getElementsByClassName("tab");
+            x[n].style.display = "flex";
+            // ... and fix the Previous/Next buttons:
+            if (n == 0) {
+                document.getElementById("prevBtn").style.display = "none";
+            } else {
+                document.getElementById("prevBtn").style.display = "inline";
             }
+            if (n == (x.length - 1)) {
+                document.getElementById("nextBtn").innerHTML = "S'inscrire";
+            } else {
+                document.getElementById("nextBtn").innerHTML = "Suivant";
+            }
+            // ... and run a function that displays the correct step indicator:
+            fixStepIndicator(n)
         }
-        // If the valid status is true, mark the step as finished and valid:
-        if (valid) {
-            document.getElementsByClassName("step")[currentTab].className += " finish";
-        }
-        return valid; // return the valid status
-    }
 
-    function fixStepIndicator(n) {
-        // This function removes the "active" class of all steps...
-        var i, x = document.getElementsByClassName("step");
-        for (i = 0; i < x.length; i++) {
-            x[i].className = x[i].className.replace(" active", "");
+        function nextPrev(n) {
+            // This function will figure out which tab to display
+            var x = document.getElementsByClassName("tab");
+            // Exit the function if any field in the current tab is invalid:
+            if (n == 1 && !validateForm()) return false;
+            // Hide the current tab:
+            x[currentTab].style.display = "none";
+            // Increase or decrease the current tab by 1:
+            currentTab = currentTab + n;
+            // if you have reached the end of the form... :
+            if (currentTab >= x.length) {
+                //...the form gets submitted:
+                document.getElementById("regForm").submit();
+                return false;
+            }
+            // Otherwise, display the correct tab:
+            showTab(currentTab);
         }
-        //... and adds the "active" class to the current step:
-        x[n].className += " active";
-    }
-</script>
+
+        function validateForm() {
+            // This function deals with validation of the form fields
+            var x, y, i, valid = true;
+            x = document.getElementsByClassName("tab");
+            y = x[currentTab].getElementsByTagName("input");
+            // A loop that checks every input field in the current tab:
+            for (i = 0; i < y.length; i++) {
+                // If a field is empty...
+                if (y[i].value == "") {
+                    // add an "invalid" class to the field:
+                    y[i].className += " invalid";
+                    // and set the current valid status to false:
+                    valid = false;
+                }
+            }
+            // If the valid status is true, mark the step as finished and valid:
+            if (valid) {
+                document.getElementsByClassName("step")[currentTab].className += " finish";
+            }
+            return valid; // return the valid status
+        }
+
+        function fixStepIndicator(n) {
+            // This function removes the "active" class of all steps...
+            var i, x = document.getElementsByClassName("step");
+            for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace(" active", "");
+            }
+            //... and adds the "active" class to the current step:
+            x[n].className += " active";
+        }
+    </script>
 </body>
 
 </html>
