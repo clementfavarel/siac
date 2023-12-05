9372b64a2addb45f2472ab8e05c2eb48599fbb1f
clementfavarel <plkode.dev@protonmail.com>
2023-12-05 16:03:18 +0100

    created map and added tabbar

---
+++ src/interfaces/pages/map/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: ../auth/login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Plan | Mapollon</title>
+    <?php include('../../../assets/includes/head.php'); ?>
+    <link rel="stylesheet" href="../../../assets/css/global.css" />
+    <link rel="stylesheet" href="./map.css" />
+</head>
+
+<body>
+
+    <?php include('../../../assets/includes/tabbar.php'); ?>
+</body>
+
+</html>
