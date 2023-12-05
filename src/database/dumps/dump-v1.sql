cf39d16709fceea541d0acc76218d1c6a35ce5e7
clementfavarel <plkode.dev@protonmail.com>
2023-12-05 16:43:38 +0100

    updated dump

--- src/database/dumps/dump-v1.sql
+++ src/database/dumps/dump-v1.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 27, 2023 at 07:15 PM
--- Server version: 10.6.12-MariaDB-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.14
+-- Hôte : localhost:3306
+-- Généré le : mar. 05 déc. 2023 à 16:30
+-- Version du serveur : 10.6.12-MariaDB-0ubuntu0.22.04.1
+-- Version de PHP : 8.1.2-1ubuntu2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mapollon`
+-- Base de données : `siac`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artiste`
+-- Structure de la table `artiste`
 --
 
 CREATE TABLE `artiste` (
@@ -51,7 +51,7 @@ CREATE TABLE `artiste` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genre`
+-- Structure de la table `genre`
 --
 
 CREATE TABLE `genre` (
@@ -64,7 +64,7 @@ CREATE TABLE `genre` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `oeuvre`
+-- Structure de la table `oeuvre`
 --
 
 CREATE TABLE `oeuvre` (
@@ -82,7 +82,7 @@ CREATE TABLE `oeuvre` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reaction`
+-- Structure de la table `reaction`
 --
 
 CREATE TABLE `reaction` (
@@ -94,7 +94,7 @@ CREATE TABLE `reaction` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stand`
+-- Structure de la table `stand`
 --
 
 CREATE TABLE `stand` (
@@ -105,7 +105,7 @@ CREATE TABLE `stand` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateur`
+-- Structure de la table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
@@ -115,29 +115,35 @@ CREATE TABLE `utilisateur` (
   `email` varchar(100) NOT NULL,
   `mdp` varchar(60) NOT NULL,
   `tranche_age` varchar(5) NOT NULL,
-  `departement` varchar(50) NOT NULL,
   `metier` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `mdp`, `tranche_age`, `metier`) VALUES
+(1, 'Clément', 'FAVAREL', 'plkode.dev@protonmail.com', '$2y$10$KqSqaKrk/EGawsaIY5VpR.NbmhyP8ixkEg7sSmlTMAxsbw1G/txyy', '16-24', 'Etudiant');
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `artiste`
+-- Index pour la table `artiste`
 --
 ALTER TABLE `artiste`
   ADD PRIMARY KEY (`id`),
   ADD KEY `artiste_genre_fk` (`genre_id`);
 
 --
--- Indexes for table `genre`
+-- Index pour la table `genre`
 --
 ALTER TABLE `genre`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `oeuvre`
+-- Index pour la table `oeuvre`
 --
 ALTER TABLE `oeuvre`
   ADD PRIMARY KEY (`id`),
@@ -145,7 +151,7 @@ ALTER TABLE `oeuvre`
   ADD KEY `oeuvre_genre_fk` (`genre_id`);
 
 --
--- Indexes for table `reaction`
+-- Index pour la table `reaction`
 --
 ALTER TABLE `reaction`
   ADD PRIMARY KEY (`id`),
@@ -153,84 +159,84 @@ ALTER TABLE `reaction`
   ADD KEY `reaction_utilisateur_fk` (`utilisateur_id`);
 
 --
--- Indexes for table `stand`
+-- Index pour la table `stand`
 --
 ALTER TABLE `stand`
   ADD PRIMARY KEY (`id`),
   ADD KEY `stand_oeuvre_fk` (`oeuvre_id`);
 
 --
--- Indexes for table `utilisateur`
+-- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `artiste`
+-- AUTO_INCREMENT pour la table `artiste`
 --
 ALTER TABLE `artiste`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `genre`
+-- AUTO_INCREMENT pour la table `genre`
 --
 ALTER TABLE `genre`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `oeuvre`
+-- AUTO_INCREMENT pour la table `oeuvre`
 --
 ALTER TABLE `oeuvre`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `reaction`
+-- AUTO_INCREMENT pour la table `reaction`
 --
 ALTER TABLE `reaction`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `stand`
+-- AUTO_INCREMENT pour la table `stand`
 --
 ALTER TABLE `stand`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `utilisateur`
+-- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `artiste`
+-- Contraintes pour la table `artiste`
 --
 ALTER TABLE `artiste`
   ADD CONSTRAINT `artiste_genre_fk` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `oeuvre`
+-- Contraintes pour la table `oeuvre`
 --
 ALTER TABLE `oeuvre`
   ADD CONSTRAINT `oeuvre_artiste_fk` FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `oeuvre_genre_fk` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `reaction`
+-- Contraintes pour la table `reaction`
 --
 ALTER TABLE `reaction`
   ADD CONSTRAINT `reaction_oeuvre_fk` FOREIGN KEY (`oeuvre_id`) REFERENCES `oeuvre` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `reaction_utilisateur_fk` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `stand`
+-- Contraintes pour la table `stand`
 --
 ALTER TABLE `stand`
   ADD CONSTRAINT `stand_oeuvre_fk` FOREIGN KEY (`oeuvre_id`) REFERENCES `oeuvre` (`id`) ON UPDATE CASCADE;
